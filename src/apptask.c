f08dc62120f16de7a163a0c3c456724df671b68e
Pickman22 <none@none.com>
2019-08-07 23:06:24 -0500

    Added monitoring print satatements to look at ADC measurements.

--- src/apptask.c
+++ src/apptask.c
@@ -5,17 +5,30 @@
 #include "adc.h"
 #include "dbg.h"
 #include "uart.h"
+#include "pwm.h"
 
 void AppTask_Flash(void* params) {
   uint32_t adc_data;
+  uint16_t pwmcnt = 0;
   TickType_t last_wake_time = xTaskGetTickCount();
+  float dc = 0.;
+  uint16_t idx;
   for(;;) {
-    vTaskDelayUntil(&last_wake_time, APP_TASK_FLASH_TS);
     GPIO_LedToggle();
-//    ADC_Start();
-//    ADC_WaitConv();
-//    adc_data = ADC_Read();
-//    DBG_DEBUG("ADC: %u\n\r", adc_data);
+
+    adc_data = ADC_ReadCh(ADC_POT_CH_E);
+    dc = adc_data / 4096.;
+
+    PWM_SetDC(PWM_PH_U, 100 * dc);
+    PWM_SetDC(PWM_PH_V, 0.);
+    PWM_SetDC(PWM_PH_W, 0.);
+
+    DBG_DEBUG("PHA: %u\n\r", ADC_ReadCh(ADC_PHA_IFBK_CH_E));
+    DBG_DEBUG("PHB: %u\n\r", ADC_ReadCh(ADC_PHB_IFBK_CH_E));
+    DBG_DEBUG("PHC: %u\n\r", ADC_ReadCh(ADC_PHC_IFBK_CH_E));
+    DBG_DEBUG("POT: %u\n\r\n\r", ADC_ReadCh(ADC_POT_CH_E));
+
+    vTaskDelayUntil(&last_wake_time, APP_TASK_FLASH_TS);
   }
 }
 
