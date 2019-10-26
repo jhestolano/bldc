f30f350cbeca972905054b954ffbaf42b4709e37
Pickman22 <none@none.com>
2019-10-26 14:34:51 -0500

    Added task to monitor useful motor signals

--- hw/inc/apptask.h
+++ hw/inc/apptask.h
@@ -1,16 +1,10 @@
 #ifndef APP_TASK_H
 #define APP_TASK_H
 
-#define APP_TASK_PRINTK_TS (1000)
-#define APP_TASK_PRINTK_PRIO (1)
-#define APP_TASK_PRINTK_STACK_SIZE (50)
+#define APP_TASK_SLOG_TS (10)
+#define APP_TASK_SLOG_PRIO (1)
+#define APP_TASK_SLOG_STACK_SIZE (100)
 
-#define APP_TASK_FLASH_TS (10)
-#define APP_TASK_FLASH_PRIO (1)
-#define APP_TASK_FLASH_STACK_SIZE (50)
-
-void AppTask_PrintK(void* params);
-
-void AppTask_Flash(void* params);
+void AppTask_SLog(void* params);
 
 #endif // APP_TASK_H
--- hw/src/apptask.c
+++ hw/src/apptask.c
@@ -1,40 +1,34 @@
 #include "FreeRTOS.h"
 #include "task.h"
-
 #include "apptask.h"
 #include "adc.h"
 #include "dbg.h"
 #include "uart.h"
 #include "pwm.h"
+#include "app.h"
 
-void AppTask_Flash(void* params) {
-  uint32_t adc_data;
-  uint16_t pwmcnt = 0;
-  TickType_t last_wake_time = xTaskGetTickCount();
-  float dc = 0.;
-  uint16_t idx;
-  for(;;) {
-    GPIO_LedToggle();
+#define SLOG_SIGBUF_SIZE (5)
 
-    adc_data = ADC_ReadCh(ADC_POT_CH_E);
-    dc = adc_data / 4096.;
-
-    PWM_SetDC(PWM_PH_U, 100 * dc);
-    PWM_SetDC(PWM_PH_V, 0.);
-    PWM_SetDC(PWM_PH_W, 0.);
-
-    DBG_DEBUG("PHA: %u\n\r", ADC_ReadCh(ADC_PHA_IFBK_CH_E));
-    DBG_DEBUG("PHB: %u\n\r", ADC_ReadCh(ADC_PHB_IFBK_CH_E));
-    DBG_DEBUG("PHC: %u\n\r", ADC_ReadCh(ADC_PHC_IFBK_CH_E));
-    DBG_DEBUG("POT: %u\n\r\n\r", ADC_ReadCh(ADC_POT_CH_E));
-
-    vTaskDelayUntil(&last_wake_time, APP_TASK_FLASH_TS);
-  }
+static void _slog(uint32_t* sigbuf) {
+  sigbuf[0] = (uint32_t)App_GetVoltage(VAdcChPot_E);
+  sigbuf[1] = (uint32_t)App_GetCurrent(IfbkPhA_E);
+  sigbuf[2] = (uint32_t)App_GetCurrent(IfbkPhB_E);
+  sigbuf[3] = (uint32_t)App_GetCurrent(IfbkPhC_E);
+  sigbuf[4] = (uint32_t)App_GetPwmVoltage(PwmChA_E);
 }
 
-void AppTask_PrintK(void* params) {
+void AppTask_SLog(void* params) {
   TickType_t last_wake_time = xTaskGetTickCount();
+  uint8_t idx;
+  uint8_t *ptr;
+  uint32_t sigbuf[SLOG_SIGBUF_SIZE] = {0};
   for(;;) {
-    vTaskDelayUntil(&last_wake_time, APP_TASK_PRINTK_TS);
+    _slog(sigbuf);
+    ptr = (uint8_t*)&sigbuf[0];
+    UART_Putc(0xAA);
+    for(idx = 0; idx < sizeof(sigbuf); idx++) {
+      UART_Putc(ptr[idx]);
+    }
+    vTaskDelayUntil(&last_wake_time, APP_TASK_SLOG_TS);
   }
 }
