33ab0c93f00d285b06795ccf280e37fb6fb83ad1
Pickman22 <none@none.com>
2019-12-01 23:31:13 -0600

    Added configuration/setup code for encoder. It is working now.

--- hw/src/enc.c
+++ hw/src/enc.c
@@ -10,28 +10,45 @@
 static TIM_HandleTypeDef gs_tim_enc_conf = TIM_ENC_INIT_CONF;
 
 static void ENC_ErrorHandler(char* errmsg) {
-  DBG_ERR(errmsg);
+  DBG_ERR("ENC: %s", errmsg);
+  while(1);
 }
 
 void ENC_Init(void) {
-  DBG_DEBUG("Initializing encoder interface...");
+  DBG_DEBUG("Initializing encoder interface...\n\r");
   TIM_Encoder_InitTypeDef s_enc_conf = ENC_INIT_CONF;
   if(HAL_TIM_Encoder_Init(&gs_tim_enc_conf, &s_enc_conf) != HAL_OK) {
-    ENC_ErrorHandler("Error initializing encoder interface!");
+    ENC_ErrorHandler("Error initializing encoder interface!\n\r");
   }
+  ENC_Start();
+  DBG_DEBUG("Done.\n\r");
 }
 
 void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* s_enc_conf) {
-  
+  UNUSED(s_enc_conf);
+  GPIO_InitTypeDef s_enc_ch_a_gpio_conf = ENC_CH_A_GPIO_CONF;
+  GPIO_InitTypeDef s_enc_ch_b_gpio_conf = ENC_CH_B_GPIO_CONF;
+ 
+  if(!__HAL_RCC_TIM2_IS_CLK_ENABLED()) {
+    __HAL_RCC_TIM2_CLK_ENABLE(); 
+  } 
+  if(!__HAL_RCC_GPIOA_IS_CLK_ENABLED()) {
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+  }
+  if(!__HAL_RCC_GPIOB_IS_CLK_ENABLED()) {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+  }
+  HAL_GPIO_Init(ENC_CH_A_PORT, &s_enc_ch_a_gpio_conf);
+  HAL_GPIO_Init(ENC_CH_B_PORT, &s_enc_ch_b_gpio_conf);
 }
 
 
 void ENC_Start(void) {
-
+  HAL_TIM_Encoder_Start(&gs_tim_enc_conf, ENC_CH_ALL);
 }
 
 uint16_t ENC_GetCnt(void) {
-
+  return __HAL_TIM_GET_COUNTER(&gs_tim_enc_conf);
 }
 
 void ENC_DeInit(void) {
