b36776fd2079a06913e272b883b64fa18e282041
Pickman22 <none@none.com>
2019-12-04 22:01:23 -0600

    Cleaned up streaming task.

--- hw/src/apptask.c
+++ hw/src/apptask.c
@@ -17,9 +17,7 @@ static void _slog(uint32_t* sigbuf) {
   static uint16_t idx;
   sigbuf[0] = (uint32_t)App_GetVoltage(VAdcChPot_E);
   sigbuf[1] = (uint32_t)App_GetTemp();
-//  sigbuf[2] = TMR_GetCount();
-//  sigbuf[0] = gAdcChBuf[idx++];
-  sigbuf[2] = (uint32_t)ENC_GetCnt();
+  sigbuf[2] = (uint32_t)App_GetPosition();
   sigbuf[3] = (uint32_t)App_GetCurrent(IfbkPhC_E);
 }
 
@@ -28,21 +26,13 @@ void AppTask_SLog(void* params) {
   uint8_t idx;
   uint8_t *ptr;
   uint32_t sigbuf[SLOG_SIGBUF_SIZE] = {0};
-  uint8_t done = 0;
-  uint16_t adcidx = 0;
   for(;;) {
-//    __ADC_DISABLE_ISR();
-//    done = gFlagAdcBufFull;
-//    __ADC_ENABLE_ISR();
-//    if(done && (adcidx < (uint16_t)ADC_CH_BUF_LEN)) {
-      _slog(sigbuf);
-      ptr = (uint8_t*)&sigbuf[0];
-      UART_Putc(0xAA);
-      for(idx = 0; idx < sizeof(sigbuf); idx++) {
-        UART_Putc(ptr[idx]);
-      }
-      adcidx++;
-//    }
+    _slog(sigbuf);
+    ptr = (uint8_t*)&sigbuf[0];
+    UART_Putc(0xAA);
+    for(idx = 0; idx < sizeof(sigbuf); idx++) {
+      UART_Putc(ptr[idx]);
+    }
     vTaskDelayUntil(&last_wake_time, APP_TASK_SLOG_TS);
   }
 }
@@ -52,10 +42,7 @@ void AppTask_500ms(void* params) {
   TickType_t last_wake_time = xTaskGetTickCount();
   uint32_t idx = 0;
   for(;;) {
-//    GPIO_LedToggle();
     vTaskDelayUntil(&last_wake_time, APP_TASK_500MS);
-    // App_SetPwmVoltage(PwmChA_E, 5e3 * idx);
     idx ^= 1u;
-
   }
 }
