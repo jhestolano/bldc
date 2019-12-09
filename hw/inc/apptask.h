bdf4349cae73fbba0c61113b23ed5ab39c4782e6
Pickman22 <none@none.com>
2019-12-08 18:07:56 -0600

    added new task for motor control @1ms.

--- hw/inc/apptask.h
+++ hw/inc/apptask.h
@@ -1,6 +1,8 @@
 #ifndef APP_TASK_H
 #define APP_TASK_H
 
+#include "mtrif.h"
+
 #define APP_TASK_SLOG_TS (10)
 #define APP_TASK_SLOG_PRIO (2)
 #define APP_TASK_SLOG_STACK_SIZE (100)
@@ -9,8 +11,16 @@
 #define APP_TASK_500MS_PRIO (1)
 #define APP_TASK_500MS_STACK_SIZE (100)
 
+#define APP_TASK_MOTOR_CONTROL_TS (1)
+#define APP_TASK_MOTOR_CONTROL_PRIO (3)
+#define APP_TASK_MOTOR_CONTROL_STACK_SIZE (100)
+
 void AppTask_SLog(void* params);
 
 void AppTask_500ms(void* params);
+
+void AppTask_MotorControl(void* params);
+
+MtrIf_S* AppTask_GetMtrIf(void);
 
 #endif // APP_TASK_H
--- hw/src/apptask.c
+++ hw/src/apptask.c
@@ -8,16 +8,19 @@
 #include "app.h"
 #include "tmr.h"
 #include "gpio.h"
+#include "mtrif.h"
 #include "enc.h"
 
 #define SLOG_SIGBUF_SIZE (4)
+
+static MtrIf_S gs_mtr_if = {0};
 
 #ifdef __SLOG__
 static void _slog(uint32_t* sigbuf) {
   static uint16_t idx;
   sigbuf[0] = (uint32_t)App_GetVoltage(VAdcChPot_E);
-  sigbuf[1] = (uint32_t)App_GetTemp();
-  sigbuf[2] = (uint32_t)App_GetPosition();
+  sigbuf[1] = (uint32_t)MtrIf_CalcSpd(&gs_mtr_if);
+  sigbuf[2] = (uint32_t)MtrIf_GetPos(&gs_mtr_if);
   sigbuf[3] = (uint32_t)App_GetCurrent(IfbkPhC_E);
 }
 
@@ -45,4 +48,17 @@ void AppTask_500ms(void* params) {
     vTaskDelayUntil(&last_wake_time, APP_TASK_500MS);
     idx ^= 1u;
   }
+}
+
+void AppTask_MotorControl(void* params) {
+  TickType_t last_wake_time = xTaskGetTickCount();
+  MtrIf_Init(&gs_mtr_if);
+  for(;;) {
+    MtrIf_Upd(&gs_mtr_if);
+    vTaskDelayUntil(&last_wake_time, APP_TASK_MOTOR_CONTROL_TS); 
+  }
+}
+
+MtrIf_S* AppTask_GetMtrIf(void) {
+  return &gs_mtr_if;
 }
