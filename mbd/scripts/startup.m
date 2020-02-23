40fdc8029de9d73657ba5e248c276e99549f79e9
Pickman22 <none@none.com>
2020-02-23 16:05:22 -0600

    Deleting auto generated files

--- mbd/scripts/startup.m
+++
@@ -1,8 +0,0 @@
-clear; clc;
-proj = simulinkproject;
-Simulink.fileGenControl('set', ...
-    'CodeGenFolder', fullfile(proj.RootFolder, 'codegen'), ...)
-    'CacheFolder', fullfile(proj.RootFolder, 'cache'), ...
-    'CreateDir', true);
-motor_DD;
-ctrl_DD;
