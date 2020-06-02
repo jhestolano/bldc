0224392369804b67cf6d09c241e2791ab4726497
Pickman22 <none@none.com>
2020-06-02 16:15:46 -0500

    Adding a proper HardFault handler that retreieves stack info.

--- hw/src/stm32f3xx_it.c
+++ hw/src/stm32f3xx_it.c
@@ -74,18 +74,78 @@ void NMI_Handler(void)
 {
 }
 
+typedef struct __attribute__((packed)) ContextStateFrame {
+  uint32_t r0;
+  uint32_t r1;
+  uint32_t r2;
+  uint32_t r3;
+  uint32_t r12;
+  uint32_t lr;
+  uint32_t return_address;
+  uint32_t xpsr;
+} ContextStateFrame_S;
+
+
+/* Avoid optimization so that State Frame is not opmtimized away. */
+__attribute__((optimize("O0")))
+/* void debugHardfault(ContextStateFrame_S *frame) */
+void debugHardfault(uint32_t* sp)
+{
+
+  ContextStateFrame_S *frame = (ContextStateFrame_S*)sp;
+
+  uint32_t cfsr  = SCB->CFSR;
+  uint32_t hfsr  = SCB->HFSR;
+  uint32_t mmfar = SCB->MMFAR;
+  uint32_t bfar  = SCB->BFAR;
+
+  uint32_t r0  = sp[0];
+  uint32_t r1  = sp[1];
+  uint32_t r2  = sp[2];
+  uint32_t r3  = sp[3];
+  uint32_t r12 = sp[4];
+  uint32_t lr  = sp[5];
+  uint32_t pc  = sp[6];
+  uint32_t psr = sp[7];
+
+  printf("HardFault:\n");
+  printf("SCB->CFSR   0x%08lx\n", cfsr);
+  printf("SCB->HFSR   0x%08lx\n", hfsr);
+  printf("SCB->MMFAR  0x%08lx\n", mmfar);
+  printf("SCB->BFAR   0x%08lx\n", bfar);
+  printf("\n");
+
+  printf("SP          0x%08lx\n", (uint32_t)sp);
+  printf("R0          0x%08lx\n", r0);
+  printf("R1          0x%08lx\n", r1);
+  printf("R2          0x%08lx\n", r2);
+  printf("R3          0x%08lx\n", r3);
+  printf("R12         0x%08lx\n", r12);
+  printf("LR          0x%08lx\n", lr);
+  printf("PC          0x%08lx\n", pc);
+  printf("PSR         0x%08lx\n", psr);
+  __asm("BKPT #0\n");
+  while(1);
+}
+
 /**
   * @brief  This function handles Hard Fault exception.
   * @param  None
   * @retval None
   */
+__attribute__((naked))
 void HardFault_Handler(void)
 {
-  /* Go to infinite loop when Hard Fault exception occurs */
-  DBG_ERR("Fatal: HardFault_Handler called!");
-  while (1)
-  {
-  }
+  __asm volatile
+    (
+     "tst lr, #4                                    \n"
+     "ite eq                                        \n"
+     "mrseq r0, msp                                 \n"
+     "mrsne r0, psp                                 \n"
+     "ldr r1, debugHardfault_address                \n"
+     "bx r1                                         \n"
+     "debugHardfault_address: .word debugHardfault  \n"
+    );
 }
 
 /**
