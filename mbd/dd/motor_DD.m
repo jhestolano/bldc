6acacfe4bc61ff7a0ba3473963d55382dd29b0cf
pickman22 <jhestolano@gmail.com>
2020-10-04 14:25:18 -0500

    Add observer tuning.
    ADRC observer is tuned using pole placement method.

--- mbd/dd/motor_DD.m
+++ mbd/dd/motor_DD.m
@@ -24,3 +24,32 @@ CtrlParams.PosKi = 0.0;
 SmDiffParams.AccMax = 9e4;
 SmDiffParams.Ki = 1.1 * SmDiffParams.AccMax;
 SmDiffParams.Kp = sqrt(SmDiffParams.AccMax);
+
+%% Disturbance observer tuning.
+damp_fctr = 0.99;
+Tsettle = 1e-3; % 2% Rule.
+
+% Observer form:
+% e = z1 - y;
+% dz1/dt = z1 - k1 * e;
+% dz2/dt = z2 + b * u - K2 * e;
+% dz3/dt = z3 - k3 * e;
+
+wn = 6 / (damp_fctr * Tsettle);
+s1 = -damp_fctr * wn + wn * sqrt(1 - damp_fctr^2) * 1j;
+s2 = -damp_fctr * wn - wn * sqrt(1 - damp_fctr^2) * 1j;
+s3 = -800 * damp_fctr;
+
+A = [0, 1, 0;
+     0, 0, 1;
+     0, 0, 0];
+
+C = [1, 0, 0];
+
+K = place(A', C', [s1, s2, s3])';
+AdrcParams.K1 = K(1);
+AdrcParams.K2 = K(2);
+AdrcParams.K3 = K(3);
+AdrcParams.Ts = 1/30.e3;
+
+clear damp_fctr, wn, s1, s2, s3, A, C, K;
