5f8be694e483c746a670f78fa53b7332a2fd2241
pickman22 <jhestolano@gmail.com>
2020-11-01 14:02:22 -0600

    Add autocalibration routine.
    At startup the current controller autocalibration runs which
    identifies the inductance and the resistance of the motor.
    
    Each state of the autocal routine is defined with an
    enumeration AutoCalSt.
    
    TODO: calculate the appropriate controller gains and update
    the controller if successful.

---
+++ mbd/dd/AutoCalSt.m
@@ -0,0 +1,16 @@
+classdef AutoCalSt < Simulink.IntEnumType
+    % AUTOCALST Auto-Calibration State.
+    %   Defines the auto-calibration status/state.
+    enumeration
+      ErrIndTooSmall(-1),
+      ErrIndTooLarge(-2),
+      ErrResTooSmall(-3),
+      ErrResTooLarge(-4),
+      NotStarted(0),
+      IndId(1);
+      IndIDCoolDown(2),
+      ResID(3)
+      Done(255),
+    end
+end
+
